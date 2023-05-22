acac6978082c161280afc5969406d74f8dfe54ca
ОльгаЛук2911 <olya.lukash291991@mail.ru>
2023-05-22 17:21:50 +0300

    week_13

---
+++ index.js
@@ -0,0 +1,43 @@
+//1
+let cleaner = 40000;
+let teacher = 22000;
+console.log(cleaner+teacher);
+
+//2
+let nameGroup = 'Владивосток ';
+let number = 2000;
+console.log(nameGroup+number);
+
+//3
+let messenger = 128;
+let photo = 254;
+let Vkontakte = 137;
+let YouTube = 201;
+console.log((messenger+photo+Vkontakte+YouTube)/60);
+
+//4
+let x = 2;
+console.log(x**5);
+
+//5
+console.log('"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"');
+
+//6
+const greetings = "Привет";
+const name = "Кот";
+console.log(`${greetings}, ${name}`)
+
+//7
+let time = 34;
+console.log('Старт поездки. Осталось минут: '+ time);
+time -= 15;
+console.log('Немного сторис в соцсетях. Осталось минут: '+ time);
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+//8
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
